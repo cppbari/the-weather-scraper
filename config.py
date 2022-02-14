150caa4cf15dd29bb9da53797233af6e3e3b5e3f
Brian Thomas <bthomas1@wsu.edu>
2022-02-14 12:33:23 -0800

    Updated settings with new stations.

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ from datetime import date
 
 # Set Date format like: YYYY, MM, DD
 # Note that FIND_FIRST_DATE uses START_DATE as default start date
-START_DATE = date(1990, 1, 1)
-END_DATE = date(2020, 7, 19)
+START_DATE = date(2022, 1, 1)
+END_DATE = date(2022, 2, 14)
 
 # set to "metric" or "imperial"
 UNIT_SYSTEM = "metric"
 # UNIT_SYSTEM = "imperial"
 
 # Automatically find first date where data is logged
-FIND_FIRST_DATE = True
+FIND_FIRST_DATE = False
